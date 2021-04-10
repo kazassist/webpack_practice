c5c3b8d75d3c157f4553f09455d6c4b8a6cf5052
Kazuhiro Nigara <12196192+kazassist@users.noreply.github.com>
2021-04-10 14:12:27 +0900

    Generate HTML through Pug

--- webpack.config.js
+++ webpack.config.js
@@ -27,15 +27,20 @@ module.exports = {
         type: 'asset/resource',
         generator: {
           filename: 'images/[name][ext]'
-        },
+        }
+      },
+      {
+        test: /\.pug/,
         use: [
-          // {
-          //   loader: 'file-loader',
-          //   options: {
-          //     esModule: false,
-          //     name: 'images/[name].[ext]'
-          //   }
-          // }
+          {
+            loader: 'html-loader'
+          },
+          {
+            loader: 'pug-html-loader',
+            options: {
+              pretty: true
+            }
+          }
         ]
       }
     ]
@@ -45,7 +50,12 @@ module.exports = {
       filename: './stylesheets/main.css'
     }),
     new HtmlWebpackPlugin({
-      template: './src/templates/index.html'
+      template: './src/templates/index.pug',
+      filename: 'index.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/templates/access.pug',
+      filename: 'access.html'
     }),
     new CleanWebpackPlugin()
   ]
